84473734daacc87abb68195e3a5cc94a0f7a8a9d
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2024-01-26 00:31:19 +0700

    xử lý game

--- game.py
+++ game.py
@@ -15,13 +15,15 @@ player = pygame.image.load('img/player.png')
 player = pygame.transform.scale(player,(100, 50))
 player_rect = player.get_rect()
 player_rect.x = 0
-player_rect.y = 0
+player_rect.y = SCREEN_HEIGHT/2
+speed = 2
     # bullet
 bullet = pygame.image.load('img/bullet.png')
 bullet = pygame.transform.scale(bullet, (80,30))
 bullet_rect = bullet.get_rect()
 bullet_rect.x = 100
 bullet_rect.y = 100
+speed_bullet = 1
     # gem
 gem = pygame.image.load('img/gem.png')
 gem = pygame.transform.scale(gem,(90, 50))
@@ -31,6 +33,10 @@ gem_rect.y = 300
 # score: surface text
 f_game = pygame.font.Font('./fonts/font_game.otf', 32)
 score = 0
+score_title = f_game.render(f'Score {score}', True, 'Red', 'White')
+score_title_rect = score_title.get_rect()
+score_title_rect.x = SCREEN_WIDTH - score_title.get_width() - 10
+score_title_rect.y = 10
 # background
 bg = pygame.image.load('./img/bg_game.jpg')
 bg = pygame.transform.scale(bg, (SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -42,16 +48,31 @@ while running:
     for event in events:
         if event.type == pygame.QUIT:
             running = False
+        # Xử lý bắn
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bullet_rect.x = player_rect.x
+                bullet_rect.y = player_rect.y
     # Kịch bản game
+    key = pygame.key.get_pressed()
+    if key[pygame.K_UP] and player_rect.y > 50:
+        player_rect.y -= speed
+    elif key[pygame.K_DOWN] and player_rect.y < SCREEN_HEIGHT - player_rect.height:
+        player_rect.y += speed
+    if key[pygame.K_LEFT] and player_rect.x > 0:
+        player_rect.x -= speed
+    elif key[pygame.K_RIGHT] and player_rect.x < SCREEN_WIDTH - player_rect.width:
+        player_rect.x += speed
+    # Xử lý đạn
+    bullet_rect.x += speed_bullet
     # screen.fill((255,255,255))
     screen.blit(bg,(0,0))
     screen.blit(player, player_rect)
     screen.blit(bullet, bullet_rect)
     screen.blit(gem, gem_rect)
     # score
-    score_title = f_game.render(f'Score {score}', True, 'Red', 'White')
-    screen.blit(score_title, (SCREEN_WIDTH - score_title.get_width(), 0))
-
+    score_title
+    screen.blit(score_title, (score_title_rect))
     # Cập nhật game
     pygame.display.flip()
 
